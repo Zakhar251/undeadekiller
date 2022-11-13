3a7a7d6f924375ef5f51fa242c05a5f68dbed0d8
Zakhar251 <118106583+Zakhar251@users.noreply.github.com>
2022-11-13 20:31:26 +0300

    rgsrgsges

--- NPC.gd
+++ NPC.gd
@@ -1,6 +1,6 @@
 extends KinematicBody2D
 
-const SPEED =100
+const SPEED =50
 const GRAVITY = 100
 const FLOOR = Vector2(0 , -1)
 onready var hp = 30
@@ -19,12 +19,9 @@ func handle_hit(damage: int):
 	print("enemy was hit! current health:" + str(hp))
 	if hp <1:
 		is_alive = false
-		vel.x = 0
+		vel.x = 0		
+		$Area2D/AnimationPlayer.play("death")
 		
-		$Goblin.play("death")
-		
-
-
 func _der():
 		der *= -1
 		$Goblin.flip_h = !$Goblin.flip_h
@@ -36,14 +33,6 @@ func _physics_process(delta):
 	move_and_slide(vel, FLOOR)
 	if is_on_wall():
 		_der()
-	
-		
-	
-	
-	
-	
-	
-
 
 func _on_Goblin_animation_finished():
 	if $Goblin.animation == "death":
