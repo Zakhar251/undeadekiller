a3ac34a7f8095180478960875f18443fcbdafd0d
Gilog0 <118125416+Gilog0@users.noreply.github.com>
2022-11-20 16:08:18 +0300

    hdmtd
    464h4

--- REVIM.gd
+++ REVIM.gd
@@ -1,11 +1,10 @@
 extends Control
 
-
-
-
-func _on_story_game_pressed():
-	get_tree().change_scene("res://main.tscn")
-
-
 func _on_Button_pressed():
 	get_tree().change_scene("res://Menu.tscn")
+	
+	
+
+
+func _on_StoryGame_pressed():
+	get_tree().change_scene("res://main.tscn")
